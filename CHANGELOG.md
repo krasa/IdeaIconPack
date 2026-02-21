571f26fa2b37c7a9f3f1249a16b647a6657fed9f
Vojtěch Krása <vojta.krasa@gmail.com>
2026-02-21 15:40:54 +0100

    gradle build

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## [Unreleased]
+
+- New option - Icons from 2018.1 (Files icons only) 
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "Legacy Icon Pack"
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
